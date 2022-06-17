2da6467f66cc8396df66adfd8e617f6f8924d4b2
Eingest <ak0805@live.de>
2022-06-17 11:50:30 +0200

    Initial Commit

---
+++ TabViewProject/TabViewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewProjectApp.swift
+//  TabViewProject
+//
+//  Created by Andreas Kiesel on 17.06.22.
+//
+
+import SwiftUI
+
+@main
+struct TabViewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
